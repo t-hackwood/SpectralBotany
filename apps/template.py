20322944ed42c50bc2863eb133cd6ede25a4954e
t-hackwood <thackwood@TimOzius>
2024-08-29 11:13:02 +1000

    Initial commit

---
+++ apps/template.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+import os
+import numpy as np
+from osgeo import gdal, ogr, osr
+from rios import applier, cuiprogress
+
+
+
+
+
+
+
+
+
+def main(options, infiles, outfiles):
+
+
+if __name__ == '__main__':
+    options = 
+    infiles =
+    outfiles =
+    main(options, infiles, outfiles)
