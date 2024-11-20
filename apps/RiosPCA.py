cf238cfe812f2b1ce004e1beb82f0e45ecea8063
t-hackwood <thackwood@TimOzius>
2024-11-20 21:07:29 +1000

    updates

--- apps/RiosPCA.py
+++ apps/RiosPCA.py
@@ -11,26 +11,65 @@ import glob
 
 
 # FIles for PCA and Segment outputs
-RASTER_PC = "/home/tim/rubella/scripts/SpectralBotany/data/Sentinel/Sentinel_brigalow_PCA.tif" 
+RASTER_PC = "/home/tim/rubella/scripts/SpectralBotany/data/Sentinel/Sentinel_brigalow_PCA_v2.tif" 
 
 # Apply the PCA to the full image and mask to Brigalow AOI
 # Takes ~20 minutes
+# Apply the PCA to the full image and mask to Brigalow AOI
+# Takes ~20 minutes
+
+def all_ratios(sampleData):
+    numBands = sampleData.shape[0]
+    clippedData = np.clip(sampleData, 0, None).astype(float)
+    # Get indices of upper triangle of the array
+    i, j = np.triu_indices(numBands, 1)
+    # Calculate the ratio of each band to every other band
+    ratio_arr = (clippedData[i]-clippedData[j])/(clippedData[i]+clippedData[j]+np.finfo(float).eps)
+    # Concatenate sampleData and reshaped ratio_arr
+    sampleData = np.concatenate((sampleData,ratio_arr.reshape((-1,) + sampleData.shape[1:])), axis=0)
+    
+    return sampleData
+
+
+
 def _applyPCA(info, inputs, outputs, otherargs):
     """
     Apply PCA to full resolution dataset.
     """
     # Open the images
-    img = inputs.raster
-    inshape = img.shape
+    s2 = inputs.inlist
+    inshape = s2[0].shape
     aoi = inputs.aoi
-    
-    # Get indicies
-    ndvi = (img[3]-img[2])/(img[3]+img[2]+np.finfo(float).eps).reshape(1, inshape[1], inshape[2])
-    ndwi = (img[1]-img[5])/(img[1]+img[5]+np.finfo(float).eps).reshape(1, inshape[1], inshape[2])
+    # nodata = np.any(s2[0] == 0, axis=0)
 
+    datastack = []
+
+    for img in s2:
+
+        img = all_ratios(img)
         
-    # Stack these ratios with the original data
-    stack = np.vstack([img, ndvi, ndwi])
+        inshape = img.shape        
+        ndvi = (img[3]-img[2])/(img[3]+img[2]+np.finfo(float).eps).reshape(1, inshape[1], inshape[2])
+        ndwi = (img[1]-img[5])/(img[1]+img[5]+np.finfo(float).eps).reshape(1, inshape[1], inshape[2])
+            
+        # Stack these ratios with the original data
+        stack = np.vstack([img, ndvi, ndwi])
+        
+        datastack.append(stack)
+            
+    stack = np.vstack(datastack)
+
+    # Calculate the mean and std across all images (axis 0) for each band
+    # mean_bands = np.mean(datastack, axis=0)
+    # std_bands = np.std(datastack, axis=0)
+    # stdOut = 10000 * 10000 + std_bands
+    # print(stdOut.shape)
+    # stdOut[:, np.any(nodata)] = 0
+    # outputs.ndviSD = stdOut.astype(np.uint16)
+
+    # # Stack the mean and std arrays to form a new data stack
+    # stack = np.concatenate([mean_bands, std_bands], axis=0)
+
     scaled_stack = np.reshape(stack, (stack.shape[0], -1)).astype('float32').T
 
     # Apply the PCA    
@@ -46,11 +85,11 @@ def _applyPCA(info, inputs, outputs, otherargs):
     # Mask the output for no data
 
     pc[:,np.any(stack == otherargs.noData,axis=0)] = 0
-    pc[:, np.any(aoi == 255, axis=0)] = 0 # mask to the AOI
+    pc[:, np.any(aoi == 0, axis=0)] = 0 # mask to the AOI
     outputs.pc =  pc.astype(np.uint16)
 
 # Get the no data value
-ds = gdal.Open("/home/tim/dentata/Sentinel2_seasonal/cvmsre_qld_m202312202402_abma2.vrt")
+ds = gdal.Open("/home/tim/dentata/Sentinel2_seasonal/cvmsre_qld_m202109202111_abma2.vrt")
 noData = ds.GetRasterBand(1).GetNoDataValue()
     
 # Create the RIOS file objects
@@ -58,10 +97,11 @@ infiles = applier.FilenameAssociations()
 outfiles = applier.FilenameAssociations()
 
 # Setup the IO
-infiles.aoi = "/home/tim/rubella/scripts/SpectralBotany/data/BBSaoi.tif"
-infiles.raster = "/home/tim/dentata/Sentinel2_seasonal/cvmsre_qld_m202312202402_abma2.vrt"
+infiles.inlist = ["/home/tim/dentata/Sentinel2_seasonal/cvmsre_qld_m202309202311_abma2_brig.tif"]
+infiles.aoi = "/home/tim/rubella/scripts/SpectralBotany/data/SpectralBotanyTest.gpkg"
 
 outfiles.pc = RASTER_PC
+# outfiles.ndviSD = "./NDVI_SD"
 
 # Get the otherargs
 otherargs = applier.OtherInputs()
@@ -72,13 +112,14 @@ otherargs.noData = noData
 
 # Controls for the processing   
 controls = applier.ApplierControls()
+controls.vectorlayer = "BrigalowBeltAOI"
+controls.setBurnValue = 1
 controls.windowxsize = 512
 controls.windowysize = 512
-controls.setReferenceImage(referenceImage="aoi")
-controls.setFootprintType("INTERSECTION")
-controls.setResampleMethod("near")
 controls.setStatsIgnore(0) #  nodata
 controls.progress = cuiprogress.CUIProgressBar()
+controls.setFootprintType("INTERSECTION")
+controls.setResampleMethod("near")
 controls.setOutputDriverName("GTIFF")
 controls.setCreationOptions(["COMPRESS=DEFLATE",
                                 "ZLEVEL=9",
@@ -94,8 +135,8 @@ controls.setCreationOptions(["COMPRESS=DEFLATE",
 conc = applier.ConcurrencyStyle(numReadWorkers=3,
                                 numComputeWorkers=2,
                                 computeWorkerKind="CW_THREADS",
-                                readBufferPopTimeout=360,
-                                computeBufferPopTimeout=360
+                                readBufferPopTimeout=120,
+                                computeBufferPopTimeout=120
                                 )
 
 controls.setConcurrencyStyle(conc)
--- apps/VegDistribution.py
+++ apps/VegDistribution.py
@@ -8,7 +8,80 @@ from sklearn.decomposition import PCA
 import joblib
 from rios import applier, cuiprogress
 import glob
-import re
+import re 
+from numba import njit, jit
+
+
+@njit
+def numba_median(nbar, nodata, minObs=3):
+    """Calculate the median of a stack of images, ignoring no data values."""
+    
+    xsize, ysize, nfiles = nbar.shape
+    
+    output = np.empty((1,xsize, ysize), dtype=nbar.dtype)
+    
+    for x in range(xsize):
+        for y in range(ysize):
+            # Get the values for this pixel
+            values = nbar[x, y, :]
+            # Count the number of observations
+            nobs = np.sum(values != nodata)
+            # If there are enough observations, calculate the median
+            if nobs >= minObs:
+                output[:,x,y] = np.median(values[values != nodata])
+            else:
+                output[:,x,y] = nodata
+                            
+    return output
+
+
+@njit
+def numba_stddev(nbar, nodata, minObs=3):
+    """Calculate the standard deviation of a stack of images, ignoring no data values."""
+    
+    xsize, ysize, nfiles = nbar.shape
+    
+    output = np.empty((1,xsize, ysize), dtype=nbar.dtype)
+    
+    for x in range(xsize):
+        for y in range(ysize):
+            # Get the values for this pixel
+            values = nbar[x, y, :]
+            # Count the number of observations
+            nobs = np.sum(values != nodata)
+            # If there are enough observations, calculate the standard deviation
+            if nobs >= minObs:
+                output[:,x,y] = np.std(values[values != nodata])
+            else:
+                output[:,x,y] = nodata
+            
+    return output
+
+
+def numba_percentiles(arr, nodata, minObs=3, percentiles=[5, 25, 75, 95]):
+    xsize, ysize, nfiles = arr.shape
+    npercentiles = len(percentiles)
+    
+    median_output = np.empty((1, xsize, ysize), dtype=arr.dtype)
+    stddev_output = np.empty((1, xsize, ysize), dtype=arr.dtype)
+    percentile_output = np.empty((npercentiles, xsize, ysize), dtype=arr.dtype)
+    
+    for x in range(xsize):
+        for y in range(ysize):
+            values = arr[x, y, :]
+            nobs = np.sum(values != nodata)
+            if nobs >= minObs:
+                valid_values = values[values != nodata]
+                median_output[:, x, y] = np.median(valid_values)
+                stddev_output[:, x, y] = np.std(valid_values)
+                percentile_output[:, x, y] = np.percentile(valid_values, percentiles)
+            else:
+                median_output[:, x, y] = nodata
+                stddev_output[:, x, y] = nodata
+                percentile_output[:, x, y] = np.full(npercentiles, nodata)
+                
+    return median_output, stddev_output, percentile_output
+
 
 
 def _vegIndices(info, inputs, outputs, otherargs):
@@ -30,43 +103,37 @@ def _vegIndices(info, inputs, outputs, otherargs):
         ndvi = (img[3]-img[2])/(img[3]+img[2]+np.finfo(float).eps).reshape(1, inshape[1], inshape[2])
         ndwi = (img[1]-img[5])/(img[1]+img[5]+np.finfo(float).eps).reshape(1, inshape[1], inshape[2])
         
-        ndvi[:,nodata] = np.nan
-        ndwistack[:,nodata] = np.nan
-            
-        # Stack these ratios with the original data
+        # Convert to 16 bit
+        ndvi = 10000 + 10000 * ndvi
+        ndwi = 10000 + 10000 * ndwi
+        
+        # Clip to np.uint16 values
+        ndvi = np.clip(ndvi, 0, 65534).astype(np.uint16)
+        ndwi = np.clip(ndwi, 0, 65534).astype(np.uint16)
+        
+        ndvi[:,nodata] = 0
+        ndvi[aoi == 0] = 0
+        ndwi[:,nodata] = 0
+        ndwi[aoi == 0] = 0
+        
         ndvistack.append(ndvi)
-        ndwistack.append(ndwi)
+        ndwistack.append(ndwi)       
             
     ndvistack = np.stack(ndvistack, axis=0)
     ndwistack = np.stack(ndwistack, axis=0)
-
-    # Get new stack with 5th, 50th and 95th percentiles and standard deviation
-    ndviStats = np.nanpercentile(ndvistack, [5, 50, 95], axis=0)
-    ndviStats = np.squeeze(ndviStats)
-    ndviSTD = np.nanstd(ndvistack, axis=0)
-    ndvirange = np.max(ndvistack, axis=0) - np.min(ndvistack, axis=0)
-    ndviStack = np.vstack([ndviStats, ndviSTD, ndvirange])
-
-    ndwiStats = np.nanpercentile(ndwistack, [5, 50, 95], axis=0)
-    ndwiStats = np.squeeze(ndwiStats)
-    ndwiSTD = np.nanstd(ndwistack, axis=0)
-    ndwirange = np.max(ndwistack, axis=0) - np.min(ndwistack, axis=0)
-    ndwiStack = np.vstack([ndwiStats, ndwiSTD, ndwirange])
     
-    # Convert nans back to no data
-    ndviStack[np.isnan(ndviStack)] = 0
-
-    # rescale to 16bit
-    ndviStack = 10000 * 10000 + ndviStack
-    ndwiStack = 10000 * 10000 + ndwiStack
-
-    ndviStack[:,np.any(s2 == otherargs.noData,axis=0)] = 0
-    ndviStack[:, np.any(aoi == 0, axis=0)] = 0 # mask to the AOI
-    outputs.ndvi =  ndviStack.astype(np.uint16)
-
-    ndwiStack[:,np.any(s2 == otherargs.noData,axis=0)] = 0
-    ndwiStack[:, np.any(aoi == 255, axis=0)] = 0 # mask to the AOI
-    outputs.ndwi =  ndwiStack.astype(np.uint16)
+    # Transpose to x, y, time
+    ndvistack = np.transpose(np.squeeze(ndvistack), (1, 2, 0))
+    ndwistack = np.transpose(np.squeeze(ndwistack), (1, 2, 0))
+    
+    ndvimedian, ndvistd, ndviperc = numba_percentiles(ndvistack, 0)
+    ndwimedian, ndwistd, ndwiperc = numba_percentiles(ndwistack, 0)
+    
+    ndviStats = np.vstack((ndvimedian, ndvistd, ndviperc))
+    ndwiStats = np.vstack((ndwimedian, ndwistd, ndwiperc))
+    
+    outputs.ndvi =  ndviStats.astype(np.uint16)
+    outputs.ndwi =  ndwiStats.astype(np.uint16)
 
 # Get the no data value
 ds = gdal.Open("/home/tim/dentata/Sentinel2_seasonal/cvmsre_qld_m202109202111_abma2.vrt")
@@ -74,7 +141,7 @@ noData = ds.GetRasterBand(1).GetNoDataValue()
 res = ds.GetGeoTransform()[1]
 
 # Use glob to get all .vrt files
-all_files = glob.glob("/home/tim/dentata/Sentinel2_seasonal/*.tif")
+all_files = glob.glob("/home/tim/dentata/Sentinel2_seasonal/*_brig.tif")
 
 # Filter the list to include only those filenames that contain the years 2019-2024
 pattern = re.compile(r'20(19|20|21|22|23)')
@@ -105,7 +172,7 @@ controls.windowysize = 512
 controls.setStatsIgnore(0) #  nodata
 controls.progress = cuiprogress.CUIProgressBar()
 controls.setReferenceImage(referenceImage="aoi")
-controls.setFootprintType("BOUNDS_FROM_REFERENCE")
+controls.setFootprintType("INTERSECTION")
 controls.setResampleMethod("near")
 controls.setOutputDriverName("GTIFF")
 controls.setCreationOptions(["COMPRESS=DEFLATE",
@@ -122,8 +189,9 @@ controls.setCreationOptions(["COMPRESS=DEFLATE",
 conc = applier.ConcurrencyStyle(numReadWorkers=3,
                                 numComputeWorkers=2,
                                 computeWorkerKind="CW_THREADS",
-                                readBufferPopTimeout=600,
-                                computeBufferPopTimeout=600
+                                readBufferPopTimeout=300,
+                                computeBufferPopTimeout=300,
+                                readBufferInsertTimeout=300
                                 )
 
 controls.setConcurrencyStyle(conc)
